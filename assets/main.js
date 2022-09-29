3f7ba1aa507c2e497d32a9074ca3abb6663229ea
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-09-29 14:30:39 -0700

    styles project card hover animations

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,14 @@
-//// //// //// MAIN PAGE BUTTON ANIMATION
+//// //// //// HELPER FUNCTIONS //// //// ////
 
-// Selectors 
+// delay function
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+
+//// //// //// MAIN PAGE BUTTON ANIMATION //// //// ////
+
+// SELECTORS 
 const sideButtons = document.querySelectorAll("a.sidebar-links")
 const buttonContent = document.querySelectorAll("span.button-content")
 
@@ -10,35 +18,39 @@ const portLink = document.getElementById("port-link")
 const button1 = document.getElementById("button-1")
 const button2 = document.getElementById("button-2")
 
-// helper function
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+
+// EVENT LISTENERS
+
+// portfolio button event
+const portfolioEvent = () => {
+  portLink.addEventListener("mouseover", evt => {
+    let button = evt.target;
+    button.style.width = "150px"
+
+    delay(200).then(() => {
+      button1.style.visibility = "visible"
+      button1.style.position = "initial"
+    })
+  });
 }
 
-// Event listeners
-portLink.addEventListener("mouseover", evt => {
-  let button = evt.target;
-
-  button.style.width = "150px"
-
-  delay(200).then(() => {
-    button1.style.visibility = "visible"
-    button1.style.position = "initial"
-  })
-})
-
-resumeLink.addEventListener("mouseover", evt => {
-  let button = evt.target;
-
-  button.style.width = "150px"
-
-  delay(200).then(() => {
-    button2.style.visibility = "visible"
-    button2.style.position = "initial"
-  })
-})
+// Resume button event
+const resumeEvent = () => {
+  resumeLink.addEventListener("mouseover", evt => {
+    let button = evt.target;
+    button.style.width = "150px"
+    
+    delay(200).then(() => {
+      button2.style.visibility = "visible"
+      button2.style.position = "initial"
+    })
+  });
+}
+ 
+// calling button events
 sideButtons.forEach(elements => {
-
+  portfolioEvent();
+  resumeEvent();
   elements.addEventListener("mouseleave", evt => {
     let button = evt.target;
 
@@ -51,51 +63,35 @@ sideButtons.forEach(elements => {
   })
 })
 
-//// //// //// PROJECT CARD HOVER ANIMATION
-// Selectors
+//// //// //// PROJECT CARD HOVER ANIMATION //// //// ////
+
+// SELECTORS
 const topCards = document.querySelectorAll("div.top-card")
 const bottomCards = document.querySelectorAll("div.bottom-card")
 
-// Event listeners
+// EVENT LISTENERS
 topCards.forEach(elements => {
-  const links = document.querySelectorAll("a.card-individual-links")
-  const children = document.querySelectorAll("img.card-image, h1.card-title")
-
+  // Hover event
   elements.addEventListener('mouseover', evt => {
-      let card = evt.target;
+    let card = evt.target;
+    card.style.opacity = "0"
 
-      // card.style.opacity = "0"
-      card.style.visibility = "hidden";
-
-      children.forEach(elements => {
-        if (card.children === elements) {
-          // card.style.opacity = "0"
-          elements.style.visibility = "hidden"
-        } else return;
-      })  
-  })
-})
-
-bottomCards.forEach(elements => {
-  elements.addEventListener('mouseleave', evt => {
-      topCards.forEach(elements => {
-          elements.style.visibility = "visible"
+    if (card.style.opacity === "0") {
+      delay(400).then(() => {
+        card.style.visibility = "hidden";
       })
+    }
+  }) 
+})
+// Mouse leave event
+bottomCards.forEach(elements => {
+  elements.addEventListener("mouseleave", evt => {
+    topCards.forEach(elements => {
 
+      elements.style.opacity = "100"
+      elements.style.visibility = "visible"
+
+    })
   })
 })
-
-//   elements.addEventListener('mouseleave', evt => {
-//       let card = evt.target;
-
-//       card.style.opacity = "100";
-
-//       children.forEach(elements => {
-//         elements.style.opacity = "100"
-//       })
-
-//       links.forEach(elements => {
-//           elements.style.zIndex = "1"
-//       })
-//   })
 
