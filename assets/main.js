8465717160dbd4ab8c9942d90d243b099f1bb4b2
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-09-28 18:48:08 -0700

    creates hover effect for project cards, beta

--- assets/main.js
+++ assets/main.js
@@ -2,33 +2,49 @@
 
 
 // Selectors
-const topCard = document.querySelectorAll("div.top-card")
+const topCards = document.querySelectorAll("div.top-card")
+const bottomCards = document.querySelectorAll("div.bottom-card")
 
 // Event listeners
-topCard.forEach(elements => {
+topCards.forEach(elements => {
   const links = document.querySelectorAll("a.card-individual-links")
   const children = document.querySelectorAll("img.card-image, h1.card-title")
 
   elements.addEventListener('mouseover', evt => {
       let card = evt.target;
 
-      card.style.opacity = "0";
+      // card.style.opacity = "0"
+      card.style.visibility = "hidden";
 
       children.forEach(elements => {
         if (card.children === elements) {
-          elements.style.opacity = "0"
+          // card.style.opacity = "0"
+          elements.style.visibility = "hidden"
         } else return;
-      })
-  })
-  elements.addEventListener('mouseleave', evt => {
-      let card = evt.target;
-
-      card.style.opacity = "100";
-
-      children.forEach(elements => {
-        elements.style.opacity = "100"
-      })
+      })  
   })
 })
 
+bottomCards.forEach(elements => {
+  elements.addEventListener('mouseleave', evt => {
+      topCards.forEach(elements => {
+          elements.style.visibility = "visible"
+      })
+
+  })
+})
+
+//   elements.addEventListener('mouseleave', evt => {
+//       let card = evt.target;
+
+//       card.style.opacity = "100";
+
+//       children.forEach(elements => {
+//         elements.style.opacity = "100"
+//       })
+
+//       links.forEach(elements => {
+//           elements.style.zIndex = "1"
+//       })
+//   })
 
